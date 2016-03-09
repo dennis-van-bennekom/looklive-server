8c10cfad752a4672e810682c9a56ba13352c095d
Dennis van Bennekom <vanbennekomdennis@gmail.com>
2016-03-09 10:34:36 +0100

    jQuery verwijderd, BEM toegepast, header image geoptimaliseerd met srcset, html semantisch gemaakt

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,33 @@
-$(document).ready(function(){
-    $('.product').first().addClass('product-active');
-    $(".product-indicator[data-uuid='"+$('.product').first().data('uuid')+"']").addClass('product-indicator-active');
+/*
+ * jQuery specifieke code omgezet naar vanilla javascript zodat de jQuery
+ * library niet in hoeft geladen te worden. Dit scheelt een stuk in laadtijd.
+**/
+document.addEventListener('DOMContentLoaded', function() {
+    var products = document.querySelectorAll('.product');
 
-    $('.product-indicator').on('click', function(e){
-        var id = $(e.currentTarget).data('uuid');
-        $('.product-indicator-active').removeClass('product-indicator-active');
-        $(e.currentTarget).addClass('product-indicator-active');
-        $('.product.product-active').removeClass('product-active');
-        $(".product[data-uuid='"+id+"']").addClass('product-active');
+    products[0].classList.add('product-active');
+
+    var firstIndicator = document.querySelector('.product-indicator[data-uuid="' + products[0].dataset.uuid + '"]');
+
+    firstIndicator.classList.add('product-indicator-active');
+
+    var indicators = document.querySelectorAll('.product-indicator');
+
+    [].forEach.call(indicators, function(indicator) {
+        indicator.addEventListener('click', function(event) {
+            var id = event.target.dataset.uuid;
+
+            var currentIndicatorActive = document.querySelector('.product-indicator-active');
+            currentIndicatorActive.classList.remove('product-indicator-active');
+
+            var newIndicatorActive = event.target;
+            newIndicatorActive.classList.add('product-indicator-active');
+
+            var currentActive = document.querySelector('.product-active');
+            currentActive.classList.remove('product-active');
+
+            var newActive = document.querySelector('.product[data-uuid="' + id + '"]');
+            newActive.classList.add('product-active');
+        });
     });
-});
+}, false);
